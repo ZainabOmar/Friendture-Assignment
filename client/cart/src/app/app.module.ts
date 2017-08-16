5055fd634ad4a06761af9fcf765a160be2c20e49
zainab <zainab.hammami.zh@gmail.com>
2017-08-16 21:01:06 +0300

    creating users component

--- client/cart/src/app/app.module.ts
+++ client/cart/src/app/app.module.ts
@@ -7,22 +7,26 @@ import {LocationStrategy,HashLocationStrategy} from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
-import { CartService } from './services/cart.service';
+import { CartService } from './services/cart/cart.service';
+import { UsersService } from './services/users/users.service';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
 	declarations: [
 	AppComponent,
-	CartComponent
+	CartComponent,
+	UsersComponent
 	],
 	imports: [
 	BrowserModule,
 	HttpModule,
 	RouterModule.forRoot([
+		{path: '', component: UsersComponent},
 		{path: 'cart', component: CartComponent}
 		]),
 	FlashMessagesModule
 	],
-	providers: [{provide:LocationStrategy,useClass:HashLocationStrategy}, CartService],
+	providers: [{provide:LocationStrategy,useClass:HashLocationStrategy}, CartService, UsersService],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
