e6736718db332e001d40a0b8ce8473ea8dc01888
zainab <zainab.hammami.zh@gmail.com>
2017-08-18 21:41:07 +0300

    validate quantites done for back-end

--- common/models/bike.js
+++ common/models/bike.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = function(Bike) {
+	Bike.validate('quantity', customValidator, {message: "Quantity should be less"})
 
+	function customValidator(err) {
+		if (this.quantity > 15) {
+			err()
+		}else {
+			console.log("working")
+		}
+	}
 };
--- common/models/hand-watch.js
+++ common/models/hand-watch.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = function(Handwatch) {
+	Handwatch.validate('quantity', customValidator, {message: "Quantity should be less"})
 
+	function customValidator(err) {
+		if (this.quantity > 5) {
+			err()
+		}else {
+			console.log("working")
+		}
+	}
 };
