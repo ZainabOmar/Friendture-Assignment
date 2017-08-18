aef32ebd0f8f72cda07c9089618f9cc09d12b7be
zainab <zainab.hammami.zh@gmail.com>
2017-08-18 15:31:15 +0300

    display photos for each item

--- client/cart/src/app/cart/cart.component.ts
+++ client/cart/src/app/cart/cart.component.ts
@@ -11,19 +11,34 @@ import {Router} from '@angular/router';
 export class CartComponent implements OnInit {
 
 	result : any;
-	Items: string [] = [
-	"Bag", "Hand Watch",
-	"Dress", "Laptop", 
-	"Shoes", "Headphones", 
-	"Car Charger", "Mug",
-	"Book", "Wall clock",
-	"Camer", "T-shirt",
-	"Phone case", "Gift card",
-	"Notebook", "Sunglasses",
-	"Bike", "Boots",
-	"Cap", "Blender",
-	"Teapot", "Task chair"
+	images: string[] = ["../../assets/img/1", "../../assets/img/2", "../../assets/img/3"]
+
+	public Items: Array<Object> = [
+	{title: "Bag", img: "../../assets/img/1.png"},
+	{title: "Hand Watch", img: "../../assets/img/2.png"},
+	{title: "Dress", img: "../../assets/img/3.png"}, 
+	{title: "Laptop", img: "../../assets/img/4.png"}, 
+	{title: "Headphones", img: "../../assets/img/5.png"}, 
+	{title: "Mug", img: "../../assets/img/6.png"},
+	{title: "Car Charger", img: "../../assets/img/7.png"},
+	{title: "Book", img: "../../assets/img/8.png"},
+	{title: "Wall clock", img: "../../assets/img/9.png"},
+	{title: "Cap", img: "../../assets/img/10.png"},
+	{title: "Boots", img: "../../assets/img/11.png"},
+	{title: "Camera", img: "../../assets/img/12.png"},
+	{title: "T-shirt", img: "../../assets/img/13.png"},
+	{title: "Teapot", img: "../../assets/img/14.png"},
+	{title: "Phone case", img: "../../assets/img/15.png"},
+	{title: "Gift card", img: "../../assets/img/16.png"},
+	{title: "Sunglasses", img: "../../assets/img/17.png"},
+	{title: "Notebook", img: "../../assets/img/18.png"},
+	{title: "Bike", img: "../../assets/img/19.png"},
+	{title: "Blender", img: "../../assets/img/20.png"},
+	{title: "Task chair", img: "../../assets/img/21.png"}
 	];
+
+
+
 
 	constructor(
 		private router: Router,
